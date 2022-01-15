36f6472efb5d6585df0f6d1080136a7eaeecc88c
alimanaxel <alimanaxel@gmail.com>
2022-01-15 13:04:37 +0100

    Ici on fait du JS et on parle du traitement de données

---
+++ index.js
@@ -0,0 +1,44 @@
+let data = [
+
+    {
+        pseudo: "Denis",
+        age : 33,
+        technos:["Javascript", "React", "NodeJs"],
+        admin : true,
+    }
+    ,
+    {
+        pseudo: "Samia",
+        age : 24,
+        technos:["CSS", "React", "NodeJs"],
+        admin : true,
+    }
+    ,
+    {
+        pseudo: "Nikola",
+        age : 33,
+        technos:["Php", "React", "NodeJs"],
+        admin : true,
+    }
+    ,
+];
+
+// console.log(data[0].pseudo);
+
+//----------------------------
+
+//Les structures de controle
+
+let w= 0;
+
+while (w<99){
+
+    w++;
+
+    console.log(`w est ${w}`);
+}
+
+for(i=0; i<data.length; i++){
+
+
+}
